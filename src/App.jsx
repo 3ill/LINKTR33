6da88c685ba55a4a4c3f00243b968bd92124c9e6
3illBaby <thrillanon@outlook.com>
2023-10-28 22:27:32 +0100

    Structured Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { Nav } from './components';
-import { LetsConnect, Links, Footer } from './sections';
+import Profile from './components/Profile';
+import { Hero, About, LetsConnect, Footer } from './sections';
 
 const App = () => {
   return (
     <main>
-      <Nav />
-      <LetsConnect />
-      <Links />
-      <Footer />
+      <section className="xl:padding-l wide:padding-r">
+        <Hero />
+      </section>
+      <section>
+        <Profile />
+      </section>
+      <section className="padding-x py-[100px] max-sm:pt-[60px]">
+        <About />
+      </section>
+      <section className="padding-x">
+        <LetsConnect />
+      </section>
+      <section className=" mt-[60px]">
+        <Footer />
+      </section>
     </main>
   );
 };
